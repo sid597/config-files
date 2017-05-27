f17a0998aba999c7242ae061a78f9c0aa3ccf1bd
sid597 <sid597@sid597-To-be-filled-by-O-E-M>
2017-05-27 23:46:12 +0530

    Added constructor calcDistance

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,3 +1,7 @@
+
+
+
+
 public class Planet{
 
     double xxPos;
@@ -8,8 +12,7 @@ public class Planet{
     String imgFileName;
     
     public Planet(double xP, double yP, double xV,
-                  double yV, double m, String img)
-    {
+                  double yV, double m, String img) {
         this.xxPos = xP;
         this.yyPos = yP;      
         this.yyVel = yV;
@@ -21,12 +24,36 @@ public class Planet{
     
     public Planet(Planet p) 
     {
-             this.xxPos = p.xxPos;
-             this.yyPos = p.yyPos;
-             this.xxVel = p.xxVel;
-             this.yyVel = p.yyVel;
-             this.mass = p.mass;
-             this.imgFileName = p.imgFileName;
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
     }                                                     
+   
+    /* This constructor calculates distance between two Planets.*/
+    
+    public double  calcDistance(Planet p) {   
+        
+        /*  Remember to declare the return type here its double,
+         *  Incase you have no return type then specify void. 
+         * Cite :https://stackoverflow.com/questions/15698239/java-invalid-method-declaration-return-type-required 
+         */
+    
+        return Math.sqrt(((p.xxPos - this.xxPos) * (p.xxPos - this.xxPos)) + ((p.yyPos - this.yyPos) *  (p.yyPos - this.yyPos)));
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
 
